765c570c012f2e914aee689c9686f3a7a42ed966
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-04 20:33:27 +0100

    Completed Ex3

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -3,10 +3,16 @@ namespace Exercises
 {
     public class Exercise003
     {
-        // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+        // I thought about using lists and key / value pairs, but decided to keep it simple for now.
 
-        // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+        public static readonly string[] IceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+
+        public int IceCreamCode(string iceCreamFlavour)
+        {
+            for(int i = 0; i < IceCreamFlavours.Length; i++)
+                if(IceCreamFlavours[i] == iceCreamFlavour)
+                    return i;
+            return -1;
+        }
     }
 }
