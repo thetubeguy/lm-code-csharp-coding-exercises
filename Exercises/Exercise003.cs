2f2f087b769e1b66f768754dba46aeae91bb63a3
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-05 09:50:19 +0100

    Made IceCreamFlavours a read-only property

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -5,8 +5,15 @@ namespace Exercises
     {
         // I thought about using lists and key / value pairs, but decided to keep it simple for now.
 
-        public static readonly string[] IceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+        static readonly string[] LocalIceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
+
+        // Make IceCreamFlavours a read-only property
+        public static string[] IceCreamFlavours
+        {
+            get { return LocalIceCreamFlavours; }
+
+        }
         public int IceCreamCode(string iceCreamFlavour)
         {
             for(int i = 0; i < IceCreamFlavours.Length; i++)
