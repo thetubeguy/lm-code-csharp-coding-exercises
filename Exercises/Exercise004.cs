6d46be8bc109473a1e88dd4cdf75a595431c92f7
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-06 16:09:19 +0100

    Removed unecessary variable and removed unecessary braces.

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -6,17 +6,13 @@ namespace Exercises
         public static DateTime AddGigaSecond(DateTime datetime)
         {
             double gigasec = Math.Pow(10,9); 
-            TimeSpan gigasecspan = TimeSpan.FromSeconds(gigasec);
-            DateTime maxdate = DateTime.MaxValue - gigasecspan;
+
+            DateTime maxdate = DateTime.MaxValue - TimeSpan.FromSeconds(gigasec);
 
             if (datetime <= maxdate)
-            {
                 return (datetime.AddSeconds(gigasec));
-            }
             else
-            {
                 return (new DateTime(0001, 1, 1, 0, 0, 0, 0));
-            }
         }
     }
 }
