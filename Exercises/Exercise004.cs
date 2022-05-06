87c0bea7e6b4d12fa40db385ba2d34a4ebe42937
thetubeguy <59772776+thetubeguy@users.noreply.github.com>
2022-05-06 16:11:33 +0100

    Change If statement to conditional statement.

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -9,10 +9,7 @@ namespace Exercises
 
             DateTime maxdate = DateTime.MaxValue - TimeSpan.FromSeconds(gigasec);
 
-            if (datetime <= maxdate)
-                return (datetime.AddSeconds(gigasec));
-            else
-                return (new DateTime(0001, 1, 1, 0, 0, 0, 0));
+            return datetime <= maxdate ? datetime.AddSeconds(gigasec) : new DateTime(0001, 1, 1, 0, 0, 0, 0);
         }
     }
 }
